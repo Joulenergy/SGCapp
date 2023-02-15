c8528ed41da7635526778f4c2d3fe0092f6eabcd
VRMCProg22 <118323148+VRMCProg22@users.noreply.github.com>
2023-02-15 21:14:33 +0800

    Update example_useage.py

--- example_useage.py
+++ example_useage.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from models import Product
+import sys
+import json
+import ast
 
 # e.g. one column is to type DATE  so it creates today's date:
 today = datetime.today().strftime('%Y-%m-%d')
@@ -26,9 +29,22 @@ item = (today,1,"Ur mum",4,2,6,3,7,8)
 
 # insert the data above into the table using the class method we called i
 # inserting the same data 
+# normally the thing above and below is to be put into the page used to inupt the data
+
 db.insert(item)
 
 # read all the data in the db back, and loop over the rows
-
+stuff = [][]
+count = 0
 for item in db.read():
-	print(item)
+	stuff[count].append(item)
+	if len(stuff[count]) == 9:
+		def stuff[0]
+		count = count + 1
+		
+input = ast.literal_eval(sys.argv[1])
+output = input
+output.append(stuff)
+print(json.dumps(output))
+
+sys.std.flush()
