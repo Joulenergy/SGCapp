53e5d493b4d3920fde55bb7654d66ea798c9f91c
VRMCProg22 <118323148+VRMCProg22@users.noreply.github.com>
2023-04-12 23:20:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ let data = python_process('data', (data)=>{
 
 participants = []
 int count = 0;
-participants = [new Team(1,"Ur mum",4,2,6,3,7,8,2);]
+participants = [new Team("1","Ur mum","4","2","6","3","7","8","2");]
 while (count <= data.size()/8){
   count += 1;
 
